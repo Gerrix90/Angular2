43415200091b2cdbc758eb7180dbfa2784fb57a8
Gerrix <jahja.trifunovic@gmail.com>
2017-02-28 20:32:47 +0100

    Added Router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,7 +3,13 @@ import { Component } from '@angular/core';
 @Component({
   moduleId: module.id,
   selector: 'my-app',
-  template: '<user></user>' ,
+  template: `
+  <ul>
+    <li><a routerLink="/">Home</a></li>
+    <li><a routerLink="/about">About</a></li>
+  </ul>
+  <router-outlet></router-outlet>
+  ` ,
 })
 export class AppComponent  { 
 
---
+++ src/app/component/about.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'about',
+  template: '<h3>About Component</h3>' ,
+})
+export class AboutComponent  { 
+
+}
